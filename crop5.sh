abd574275358fd9bf9d82e1b4db955be44042595
JPR <245025209+iaspidem@users.noreply.github.com>
2026-05-04 16:46:08 +0100

    implement script arguments

--- crop5.sh
+++ crop5.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Get video input file
-echo "Enter file name:"
-read filename
+#echo "Enter file name:"
+#read filename
 
 # Return info on file with FFmpeg
-ffmpeg -i data/$filename -hide_banner
+ffmpeg -i data/$1 -hide_banner
+echo "Dimensions: "$2"x"$3""
