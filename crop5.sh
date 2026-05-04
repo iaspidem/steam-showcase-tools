a4fb65887d1686371fa94f4262c43f00e24037ba
JPR <245025209+iaspidem@users.noreply.github.com>
2026-05-04 17:05:30 +0100

    implement if-else block to detect video type

--- crop5.sh
+++ crop5.sh
@@ -7,3 +7,12 @@
 # Return info on file with FFmpeg
 ffmpeg -i data/$1 -hide_banner
 echo "Dimensions: "$2"x"$3""
+
+# Detect video type
+if [ "$2" -gt "$3" ]; then
+    echo "Type: Horizontal"
+elif [ "$2" -lt "$3" ]; then
+    echo "Type: Vertical"
+else
+    echo "Type: Square"
+fi
