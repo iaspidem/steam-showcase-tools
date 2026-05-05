12853eb684983843cd3c584fb572e9a4ed59f4b9
JPR <245025209+iaspidem@users.noreply.github.com>
2026-05-05 15:11:56 +0100

    implement loop and counter for cropping

--- crop5.sh
+++ crop5.sh
@@ -22,4 +22,10 @@ if (( "$2" % 5 != 0 )); then
     echo "Error: Width not divisible by 5"
 else # Perform crop
     echo "Performing video crop"
+    c=1
+    while [ $c -le 5 ]; do # Perform crop 5 times
+        #ffmpeg -i data/$1 -vf "crop=" c"$c".gif
+        echo "Status: c"$c".gif complete"
+        c=$((c+1)) # increment counter by 1
+    done
 fi
