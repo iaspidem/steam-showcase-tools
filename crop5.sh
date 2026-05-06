c76ea4b026a75ec39e7cf350e80ddd72376c5e44
JPR <245025209+iaspidem@users.noreply.github.com>
2026-05-06 11:37:35 +0100

    implement video type checking

--- crop5.sh
+++ crop5.sh
@@ -8,18 +8,31 @@
 ffmpeg -i data/$1 -hide_banner
 echo "Dimensions: "$2"x"$3""
 
-# Detect video type
+# Detect video orientation
 if [ "$2" -gt "$3" ]; then
-    echo "Type: Horizontal"
+    echo "Orientation: Horizontal"
 elif [ "$2" -lt "$3" ]; then
-    echo "Type: Vertical"
+    echo "Orientation: Vertical"
 elif [ "$2" -eq "$3" ]; then
-    echo "Type: Square"
+    echo "Orientation: Square"
+fi
+
+# Detect video type
+if [[ $1 == *".mp4" ]]; then
+    ext=".mp4"
+    echo "Type: mp4"
+elif [[ $1 == *".gif" ]]; then
+    ext=".gif"
+    echo "Type: gif"
+else
+    echo "Error: Only .gif and .mp4 file type supported"
+    exit 1
 fi
 
 # Check video width
 if (( "$2" % 5 != 0 )); then
     echo "Error: Width not divisible by 5"
+    exit 1
 else # Perform crop
     echo
     echo "Performing video crop..."
@@ -28,13 +41,14 @@ else # Perform crop
     end=0 # end point of crop
     c=1 # counter
     while [ $c -le 5 ]; do # Perform crop 5 times
+        outfile="c${c}${ext}" # Name of output file
         echo
         echo "Crop size: "$w"x"$3""
         end=$((x+w))
         echo "Crop position: "$x"-"$end""
         #ffmpeg -i data/$1 -vf "crop=" output/c"$c".gif
-        echo "Status: c"$c".gif complete"
+        echo "Status: $outfile complete"
         c=$((c+1)) # increment counter by 1
-        x=$((x+w))
+        x=$((x+w)) # increment x position by width
     done
 fi
